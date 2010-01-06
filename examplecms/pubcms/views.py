8872fd08b896d3e167d132f240ae7e134882af2e
john <john@sensibledevelopment.com>
2010-01-06 17:00:11 +0000

    got page_detail in example app working.

--- examplecms/pubcms/views.py
+++ examplecms/pubcms/views.py
@@ -1,6 +1,17 @@
+from django.shortcuts import render_to_response
+
 from models import Page
 
 def page_detail(request, page_url, queryset):
+    print page_url
     parts = page_url.split('/')
+    parts.reverse()
+    filter_params = {}
+    field = 'slug'
+    for slug in parts:
+        filter_params[field] = slug
+        field = 'parent__%s' % field
+    print filter_params
+    page = queryset.get(**filter_params)
     
-
+    return render_to_response("pubcms/page_detail.html", { 'page': page })
