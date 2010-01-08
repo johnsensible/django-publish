902ae23be49c7757757d4a066031be72c4a8c7f8
john <john@sensibledevelopment.com>
2010-01-08 11:07:02 +0000

    Added page to show when looking at "deleted" instances

--- publish/admin.py
+++ publish/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from django.shortcuts import get_object_or_404, render_to_response
+from django.core.exceptions import PermissionDenied
+from django.contrib.admin.util import unquote
+from django import template
+from django.utils.encoding import force_unicode
 
 from models import Publishable
 from actions import publish_selected
@@ -7,6 +12,7 @@ class PublishableAdmin(admin.ModelAdmin):
     
     actions = [publish_selected]
     publish_confirmation_template = None
+    deleted_form_template = None
     
     list_display = ['__unicode__', 'publish_state']
     list_filter = ['publish_state']
@@ -29,6 +35,43 @@ class PublishableAdmin(admin.ModelAdmin):
         if obj and obj.is_public:
             return False
         return super(PublishableAdmin, self).has_delete_permission(request, obj)
+    
+    def change_view(self, request, object_id, extra_context=None):
+        # TODO override change_view to trap permission errors
+        # and determine if the object being viewed is one
+        # that is marked for deletion - if so then we want
+        # to show some sort of page to indicate this fact 
+        try:
+            return super(PublishableAdmin, self).change_view(request, object_id, extra_context)
+        except PermissionDenied:
+            return self.deleted_view(request, object_id, extra_context)
+    
+    def deleted_view(self, request, object_id, extra_context=None):
+        obj = get_object_or_404(self.queryset(request), pk=unquote(object_id))
+        
+        # can only looked at deleted public instances
+        if not obj.is_public:
+            raise PermissionDenied
+
+        opts = self.model._meta
+        app_label = opts.app_label
+
+        has_absolute_url = getattr(obj, 'get_absolute_url', None)
+
+        context = {
+            'title': 'This %s will be deleted' % force_unicode(opts.verbose_name),
+            'original': obj,
+            'opts': opts,
+            'app_label': app_label,
+            'has_absolute_url': has_absolute_url,
+        }
+        context.update(extra_context or {})
+        context_instance = template.RequestContext(request, current_app=self.admin_site.name)
+        return render_to_response(self.deleted_form_template or [
+            'admin/%s/%s/deleted_form.html' % (app_label, opts.object_name.lower()),
+            'admin/%s/deleted_form.html' % app_label,
+            'admin/deleted_form.html'
+        ], context, context_instance=context_instance)
 
     def _draft_queryset(self, db_field, kwargs):
         # see if we need to filter the field's queryset
