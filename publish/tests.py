10d5f6dee9eef4e59b6242910890799c2c2a371c
john <john@sensibledevelopment.com>
2010-01-28 13:55:36 +0000

    added test to detect when publish does not work with cached instances
    if we do a dry_run publish, followed by an actual publish on the same queryset then the queryset caching currently causes issues with recursive properties (basically setting them to None)

--- publish/tests.py
+++ publish/tests.py
@@ -629,7 +629,7 @@ if getattr(settings, 'TESTING_PUBLISH', False):
             self.child2 = Page.objects.create(parent=self.page1, slug='child2', title='Child 2')
             self.child3 = Page.objects.create(parent=self.page2, slug='child3', title='Child 3')
         
-        def test_publish_select_with_overlapping_models(self):
+        def test_publish_with_overlapping_models(self):
             # make sure when we publish we don't accidentally create
             # multiple published versions
             self.failUnlessEqual(5, Page.objects.draft().count())
@@ -640,7 +640,7 @@ if getattr(settings, 'TESTING_PUBLISH', False):
             self.failUnlessEqual(5, Page.objects.draft().count())
             self.failUnlessEqual(5, Page.objects.published().count())
 
-        def test_publish_select_with_overlapping_models_published(self):
+        def test_publish_with_overlapping_models_published(self):
             # make sure when we publish we don't accidentally create
             # multiple published versions
             self.failUnlessEqual(5, Page.objects.draft().count())
@@ -654,6 +654,42 @@ if getattr(settings, 'TESTING_PUBLISH', False):
             self.failUnlessEqual(5, Page.objects.draft().count())
             self.failUnlessEqual(5, Page.objects.published().count())
 
+        def test_publish_after_dry_run_handles_caching(self):
+            # if we do a dry tun publish in the same queryset
+            # before publishing for real, we have to make
+            # sure we don't run into issues with the instance
+            # caching parent's as None
+            self.failUnlessEqual(5, Page.objects.draft().count())
+            self.failUnlessEqual(0, Page.objects.published().count())
+            
+            draft = Page.objects.draft()
+
+            all_published = NestedSet()
+            for p in draft:
+                p.publish(dry_run=True, all_published=all_published)
+            
+            # nothing published yet
+            self.failUnlessEqual(5, Page.objects.draft().count())
+            self.failUnlessEqual(0, Page.objects.published().count())
+
+            # now publish (using same queryset, as this will have cached the instances)
+            draft.publish()
+
+            self.failUnlessEqual(5, Page.objects.draft().count())
+            self.failUnlessEqual(5, Page.objects.published().count())
+
+            # now actually check the public parent's are setup right
+            page1 = Page.objects.get(id=self.page1.id)
+            page2 = Page.objects.get(id=self.page2.id)
+            child1 = Page.objects.get(id=self.child1.id)
+            child2 = Page.objects.get(id=self.child2.id)
+            child3 = Page.objects.get(id=self.child3.id)
+
+            self.failUnlessEqual(None, page1.public.parent)
+            self.failUnlessEqual(None, page2.public.parent)
+            self.failUnlessEqual(self.page1.public, child1.public.parent)
+            self.failUnlessEqual(self.page1.public, child2.public.parent)
+            self.failUnlessEqual(self.page2.public, child3.public.parent)
 
     class TestPublishableAdmin(TransactionTestCase):
         
